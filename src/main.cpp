b055e348bd18c083f2d839f37f6049028cac82af
bryanperez06 <166549936+bryanperez06@users.noreply.github.com>
2024-04-18 14:29:59 -0500

    First Commit/test

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Adafruit_Si7021.h>
+#include <RTClib.h>
+#include <HardwareSerial.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+  Serial.print(result);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}

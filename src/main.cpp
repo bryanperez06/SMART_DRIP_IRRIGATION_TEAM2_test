8722fc0ef804e317e08eecd90fa154f065f0f7a3
bryanperez06 <166549936+bryanperez06@users.noreply.github.com>
2024-04-24 17:09:05 -0500

    Testing to see how program can console output

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,23 @@
 #include <RTClib.h>
 #include <HardwareSerial.h>
 
+
+
+RTC_DS3231 RTC;
+uint32_t result;
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-  Serial.print(result);
+  result = myFunction(2, 3);
+  
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  Serial.print(result);
 }
 
 // put function definitions here:
@@ -23,12 +28,3 @@ int myFunction(int x, int y) {
   return x + y;
 }
 
-//this is a test of changes made and then uploaded to git
-//this a ANOTHER test to see if i can upload changes from my labtop
-//This is a new branch to compare code with another branch 
-//GOtta do one more to test the branch and stage process
-//This is the last one
-//testing branch
-//test chsges
-//Test again
-//More tests

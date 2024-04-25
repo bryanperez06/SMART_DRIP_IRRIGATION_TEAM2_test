51ca126144ac9460755b7970df40b6e5eefca580
bryanperez06 <166549936+bryanperez06@users.noreply.github.com>
2024-04-25 12:43:22 -0500

    ddd

--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,24 @@
 #include <RTClib.h>
 #include <HardwareSerial.h>
 
-
+using namespace std;
 
 RTC_DS3231 RTC;
 uint32_t result;
 // put function declarations here:
-int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  result = myFunction(2, 3);
-  
-  
+   // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print(result);
+   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);     
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
 

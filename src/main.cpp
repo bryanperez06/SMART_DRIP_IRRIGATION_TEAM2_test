552cc9ba1c53a96a25ad18e968e9f40147afb97a
Bryan Perez <bryan.jaziel@gmail.com>
2024-07-16 21:31:27 -0500

    changing framework to atmelMega

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ void setup() {
 }
 
 void loop() {
-   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(5000);                      // wait for a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
   delay(5000);     
